c5dcfa92c277eb50ce64f4ea2640537e11fbdea1
VasylBihari <vasylbihari@gmail.com>
2025-06-24 14:08:45 +0300

    Update solution.sql

--- days/Day_1/solution.sql
+++ days/Day_1/solution.sql
@@ -1,3 +1,8 @@
+/*Select BorrowerName from the Borrowers table.
+Uses a LEFT JOIN to join the Books table by BookID to include all borrowers, even if they haven't checked out a book.
+Aggregates the book names (BookName) into a comma-separated list (string_agg) sorted alphabetically (ORDER BY Books.BookName).
+Groups the results by borrower name (GROUP BY BorrowerName).
+Sorts the output by borrower name in ascending order (ORDER BY Borrowers.BorrowerName).*/
 
 SELECT
 	Borrowers.BorrowerName,
