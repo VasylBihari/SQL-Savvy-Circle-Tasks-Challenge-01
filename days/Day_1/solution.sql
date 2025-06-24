3991d53a48d1328bd0e6f9260e51b66b65fff609
VasylBihari <vasylbihari@gmail.com>
2025-06-24 13:56:10 +0300

    Update solution.sql

--- days/Day_1/solution.sql
+++ days/Day_1/solution.sql
@@ -1,1 +1,8 @@
 
+SELECT
+	Borrowers.BorrowerName,
+    string_agg(Books.BookName, ', ' ORDER BY Books.BookName) AS list_books
+FROM Borrowers
+LEFT JOIN Books ON Books.BookID=Borrowers.BookID
+GROUP BY BorrowerName
+ORDER BY Borrowers.BorrowerName;
