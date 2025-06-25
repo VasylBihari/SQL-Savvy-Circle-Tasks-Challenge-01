75ce58b2577f2e116b0edb8045067af26fd4d5c5
VasylBihari <vasylbihari@gmail.com>
2025-06-25 17:08:07 +0300

    Update solution.sql

--- days/Day_4/solution.sql
+++ days/Day_4/solution.sql
@@ -1,1 +1,8 @@
 
+SELECT
+	*
+FROM product_reviews
+WHERE (review_text ILIKE '%excellent%' OR review_text ILIKE'%amazing%')
+AND review_text NOT ILIKE '%not%excellent%'
+AND review_text NOT ILIKE '%not%amazing%'
+ORDER BY review_id;
