11bec29fc2061441e5c8adf1709e02905690d4e1
VasylBihari <vasylbihari@gmail.com>
2025-06-26 14:34:59 +0300

    Update solution.sql

--- days/Day_6/solution.sql
+++ days/Day_6/solution.sql
@@ -1,1 +1,7 @@
-
+SELECT 
+	category,
+  COALESCE(MIN(CASE WHEN ps.stars >= 4 THEN pr.price END), 0) as price
+FROM products pr
+LEFT JOIN purchases ps ON pr.id=ps.product_id
+GROUP BY category
+ORDER BY category; 
