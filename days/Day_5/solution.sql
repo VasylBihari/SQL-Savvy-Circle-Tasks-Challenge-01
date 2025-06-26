5a191307b897893ed3f66d229d246a9276b1ff83
VasylBihari <vasylbihari@gmail.com>
2025-06-26 10:31:49 +0300

    Update solution.sql

--- days/Day_5/solution.sql
+++ days/Day_5/solution.sql
@@ -1,1 +1,8 @@
-
+SELECT 
+	department_name,
+    ROUND(AVG(salary),2) AS avg_salary
+FROM Employees e
+JOIN Departments d ON e.department_id=d.department_id
+GROUP BY department_name
+HAVING COUNT(employee_id) > 2
+ORDER BY avg_salary DESC;
