a20696e7327f84270a178caf4514bd967f311d77
VasylBihari <vasylbihari@gmail.com>
2025-06-27 09:42:56 +0300

    Update Day_7.sql

--- days/Day_7.sql
+++ days/Day_7.sql
@@ -1,5 +1,5 @@
-/* Day 7
-https://www.namastesql.com/coding-problem/64-penultimate-order?page=1&pageSize=10&question_type=0
+/* 
+[Day 7] (https://www.namastesql.com/coding-problem/64-penultimate-order?page=1&pageSize=10&question_type=0)
 
 You are a data analyst working for an e-commerce company, responsible for analysing customer orders to gain insights into their purchasing behaviour.
 Your task is to write a SQL query to retrieve the details of the penultimate order for each customer. 
