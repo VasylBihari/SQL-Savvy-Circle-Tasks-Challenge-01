df5165f2b070c7dcd04557af4fb96d7138e53091
VasylBihari <vasylbihari@gmail.com>
2025-07-17 11:12:24 +0300

    Create Day_7.sql

---
+++ Day_7.sql
@@ -0,0 +1,16 @@
+/*Day 7 https://www.namastesql.com/coding-problem/64-penultimate-order?page=1&pageSize=10&question_type=0
+
+You are a data analyst working for an e-commerce company, responsible for analysing customer orders to gain insights into their purchasing behaviour.
+Your task is to write a SQL query to retrieve the details of the penultimate order for each customer. 
+However, if a customer has placed only one order, you need to retrieve the details of that order instead, display the output in ascending order of customer name.
+
+Table: orders
+
+| COLUMN_NAME   | DATA_TYPE   |
+|---------------|-------------|
+| order_id      | int         |
+| order_date    | date        |
+| customer_name | varchar(10) |
+| product_name  | varchar(10) |
+| sales         | int         |  */
+
