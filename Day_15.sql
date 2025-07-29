b299572a87f605cfa46c30a3f69b35b50103040e
VasylBihari <vasylbihari@gmail.com>
2025-07-29 16:34:42 +0300

    Update Day_15.sql

--- Day_15.sql
+++ Day_15.sql
@@ -28,4 +28,17 @@ The resulting table should have the machine_id along with the average time as pr
 
 Return the result table in any order.
 */
+
 SELECT 
+    a1.machine_id,
+    AVG(a2.timestamp - a1.timestamp) AS processing_time
+FROM 
+    Activity a1
+JOIN 
+    Activity a2 
+    ON a1.machine_id = a2.machine_id 
+    AND a1.process_id = a2.process_id
+    AND a1.activity_type = 'start' 
+    AND a2.activity_type = 'end'
+GROUP BY 
+    a1.machine_id;
